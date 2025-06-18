24d710d05b9b9b6361cec8e392e2a118959aea2e
SANDRICKSON <sandroqatamadze802@gmail.com>
2025-06-18 15:53:46 +0400

    Update tests.py

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class BookAPITests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', email='test@example.com', password='testpass')
+        self.client.login(username='testuser', password='testpass')
+
+    def test_get_books_list(self):
+        url = reverse('book-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_book(self):
+        url = reverse('book-list')
+        data = {
+            "title": "Test Book",
+            "author": 1,
+            "genre": 1,
+
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
